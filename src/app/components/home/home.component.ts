f2b8d1ff44a27b3ff46693c0656f0b9134a44108
davidson124 <daferdinat@gmail.com>
2025-10-19 18:46:49 -0500

    Modoficaciones visuales, se ajusta el buscador

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -58,6 +58,54 @@ export class HomeComponent {
       description: 'Espinacas tiernas y nutritivas recién cosechadas',
       category: 'Hortalizas',
       unit: 'kg'
+    },
+    {
+      name: 'Papa',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBlRB3-6ZUdPYgGYef7-F0vaDKFQDBjH9zsw&s',
+      price: 6000,
+      description: 'papas tiernas y nutritivas recién cosechadas',
+      category: 'Verduras',
+      unit: 'kg'
+    },
+    {
+      name: 'Platano',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/PlatanosVerdesBarranquilla.jpg/1200px-PlatanosVerdesBarranquilla.jpg',
+      price: 6000,
+      description: 'platano tiernas y nutritivas recién cosechadas',
+      category: 'Hortalizas',
+      unit: 'kg'
+    },
+    {
+      name: 'Yuca',
+      image: 'https://www.agrosavia.co/media/1409/agrosavia-busca-liberar-variedades-de-yuca-para-fortalecer-agroindustria-del-pai-s_agrosavia.jpg',
+      price: 6000,
+      description: 'Espinacas tiernas y nutritivas recién cosechadas',
+      category: 'Hortalizas',
+      unit: 'kg'
+    },
+    {
+      name: 'Mandarina',
+      image: 'https://cocalni.com/wp-content/uploads/2023/12/c0bc01971ce245badf2ecb80e44210ba806ed1b937d5fa2f0e728feb0f16d4b6c3ee2b34a509a0fa545b44ad0a2fddd7923613dd28f026210d71cd_1280.jpg',
+      price: 7000,
+      description: 'Mandarina tiernas y nutritivas recién cosechadas',
+      category: 'Frutas',
+      unit: 'kg'
+    },
+    {
+      name: 'Naranja',
+      image: 'https://www.lavanguardia.com/files/image_449_220/files/fp/uploads/2022/06/26/62b85245d0ebb.r_d.241-320-9335.jpeg',
+      price: 3000,
+      description: 'Naranja tiernas y nutritivas recién cosechadas',
+      category: 'Frutas',
+      unit: 'kg'
+    },
+     {
+      name: 'Pitaya',
+      image: 'https://caribbeanexotics.com.co/wp-content/uploads/2020/12/pitahaya-producto-caribbean-exotics.png',
+      price: 8000,
+      description: 'Pitaya tiernas y nutritivas recién cosechadas',
+      category: 'Frutas',
+      unit: 'kg'
     }
   ];
 }
